267bf0bc0643df3fdf313f92a1f883e451322c12
tahsif <tahsif.cse@gmail.com>
2023-11-10 09:09:18 +0600

    Inital setup with MUI and Created Navbar

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Pages/Home";
+import App from "../App";
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App></App>,
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>,
+        }
+      ]
+    },
+  ]);
+
+export default router;
